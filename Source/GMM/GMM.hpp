c761fd9b3916e867048d70cd4d1037521c07cfe3
Max Ehrlich <max.ehr@gmail.com>
2014-03-09 13:05:00 -0400

    Updated GMM with ;preliminary class model

--- Source/GMM/GMM.hpp
+++ Source/GMM/GMM.hpp
@@ -2,5 +2,17 @@
 
 namespace LocalDescriptorAndBagOfFeature 
 {
-    void ComputeSupervector(const std::vector<std::vector<double>> &bof, std::vector<double> &supervector);
+    class GMM
+    {
+        public:
+            GMM(int numGaussians);
+            
+            void Train(const std::vector<std::vector<std::vector<double>>> &bof);
+            std::vector<double> Supervector(const std::vector<std::vector<double>> &bof);
+            
+            int NumGaussians(void) const;
+            
+        private:
+            int _NumGaussians;
+    };
 }
