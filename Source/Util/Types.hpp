f38b2756c7e0751ca0f2ae92e99ee0ef08db14aa
Max Ehrlich <max.ehr@gmail.com>
2014-03-09 13:09:55 -0400

    Added some typedefs for convenience

---
+++ Source/Util/Types.hpp
@@ -0,0 +1,8 @@
+#pragma once
+
+namespace LocalDescriptorAndBagOfFeature
+{
+    typedef std::vector<double> Histogram;
+    typedef std::vector<Histogram> BagOfFeatures;
+    typedef std::vector<BagOfFeatures> FeatureSet;
+}
