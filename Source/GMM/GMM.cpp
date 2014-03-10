bfde65d2c82c56a318a29966e27d0662487308f7
Max Ehrlich <max.ehr@gmail.com>
2014-03-09 21:44:45 -0400

    Simplified code further

--- Source/GMM/GMM.cpp
+++ Source/GMM/GMM.cpp
@@ -20,30 +20,23 @@
 
 using namespace LocalDescriptorAndBagOfFeature;
 
-inline double GMM::ComputeWeightedGaussian(const cv::Mat &x, WeightedGaussian wg)
+double GMM::ComputeWeightedGaussian(const cv::Mat &x, WeightedGaussian wg)
 {
     double weight;
     cv::Mat mean, covariance;
     std::tie(weight, mean, covariance) = wg;
     
-    cv::Mat precision;
-    cv::invert(covariance, precision);
+    cv::Mat precision = covariance.inv();    
+    double outter = std::sqrt(cv::determinant(precision) / 2.0 * M_PI);
     
-    double detp = cv::determinant(precision);
-    
-    double outter = std::sqrt(detp / 2.0 * M_PI);
-    
-    cv::Mat meanDist = x - mean;
-    cv::Mat meanDistTrans;
-    cv::transpose(meanDist, meanDistTrans);
-    
-    cv::Mat symmetricProduct = meanDistTrans * precision * meanDist; // This is a "1x1" matrix e.g. a scalar value
+    cv::Mat meanDist = x - mean;    
+    cv::Mat symmetricProduct = meanDist.t() * precision * meanDist; // This is a "1x1" matrix e.g. a scalar value
     double inner = symmetricProduct.at<double>(0,0) / -2.0;
     
     return weight * outter * std::exp(inner);
 }
 
-inline double GMM::operator ()(const cv::Mat &x) const
+double GMM::operator ()(const cv::Mat &x) const
 {
     return std::accumulate(_Gaussians.begin(), _Gaussians.end(), 0, [&x](double val, WeightedGaussian wg) { return val + ComputeWeightedGaussian(x, wg); });
 }
